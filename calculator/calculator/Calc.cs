8176942bc95c86d04463867af2969010313970ad
codervivek496 <viveksinghssm496@gmail.com>
2022-05-03 14:02:36 +0530

    addition

---
+++ calculator/calculator/Calc.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace calculator
+{
+    public class Calc
+    {
+            public void Addition(int num1, int num2)
+        {
+            int sum = num1 + num2;
+                Console.WriteLine("Addition of {0} + {1} = {2}", num1, num2, sum);
+        }
+    }
+}
---
+++ calculator/calculator/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+namespace calculator
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            //Created the object/instance if calculator class;
+            Calc objCalc = new Calc();
+
+            // Taking inputs from user
+            Console.WriteLine("Enter 1st number = ");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+
+            // Taking inputs from user
+            Console.WriteLine("Enter 2nd number = ");
+            string num = Console.ReadLine();
+            int num2 = Convert.ToInt32(num);
+
+            objCalc.Addition(num1, num2);
+            //objCalc.Sub(num1, num2);
+            //objCalc.Mul(num1, num2);
+            //objCalc.Div(num1, num2);
+        }
+    }
+}
